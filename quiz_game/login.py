d0fb54a2669ba9c9b51824c5bf577b228e9ce911
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-08-19 11:38:38 -0600

    finished login

--- quiz_game/login.py
+++ quiz_game/login.py
@@ -1,5 +1,8 @@
 import csv
+from quiz import red
+from quiz import reset
 
+# Reads profile file
 def read_file():
 
     profiles = []
@@ -22,18 +25,100 @@ def read_file():
 
     return profiles
 
+def select_profile(prompt):
+    while True:
+
+        profiles = read_file()
+
+        i = 0
+        for profile in profiles:
+            i += 1
+            print(f'{i}. {profile['username']} - {profile['status']}')
+            
+        try:
+            selection = input(prompt)
+            selected_profile = profiles[int(selection) - 1]
+        except:
+            print(f'{red}Invalid Input{reset}')
+            continue
+
+        return selected_profile
+
+def delete_profile():
+
+    profiles = []
+
+    while True:
+
+        profiles = read_file()
+
+        i = 0
+        for profile in profiles:
+            i += 1
+            print(f'{i}. {profile['username']} - {profile['status']}')
+            
+        try:
+            selection = int(input('Choose a profile to delete (Enter Number)'))
+            break
+        except:
+            print(f'{red}Invalid Input{reset}')
+            continue
+
+    match input(f"1. No \n2. Yes \nAre you sure that you wish to delete {profiles[selection - 1]['username']} (Enter Number? "):
+        case '1':
+            return None
+        case '2':
+            pass
+
+    profiles.pop(selection - 1)
+
+    with open('quiz_game/profiles.csv', 'w', newline='') as profile_file:
+        profile_writer = csv.writer(profile_file)
+
+        profile_writer.writerow(['name', 'password', 'status'])
+
+        for profile in profiles:
+            profile_writer.writerow([profile['username'],
+                                     profile['password'],
+                                     profile['status']])
+
+    
+
+
+# Creates questions
+def create_profile(is_admin):
+
+    profile = [
+        input('Enter new username: '),
+        input('Enter new password: '),
+    ]
+
+    if is_admin:
+        profile.append("Admin")
+    else:
+        profile.append("User")
+    
+    # Adds header and new profile
+    with open('quiz_game/profiles.csv', 'a', newline='') as profile_file:
+        question_writer = csv.writer(profile_file)
+        question_writer.writerow(profile)
+
+# User login menu
 def login():
-    
-    profiles = read_file()
 
-    i = 0
-    for profile in profiles:
-        i += 1
-        print(f'{i}. {profile['username']} - {profile['status']}')
-    
-    selection = input("Which profile would you like to access?")
+    while True:
 
-    selection = int(selection)
+        profiles = read_file()
 
-login()
+        if profiles:
+            selected_profile = select_profile("Choose a profile (Enter Number): ")
 
+            if input(f"What is the password for {selected_profile['username']}: ") == selected_profile['password']:
+                if selected_profile['status'] == 'Admin':
+                    return True
+                else:
+                    return False
+            else:
+                print(f'{red}Incorrect Password{reset}')
+        else:
+            create_profile(True)
--- quiz_game/main.py
+++ quiz_game/main.py
@@ -6,6 +6,9 @@ from quiz import reset
 from quiz_create import create_quiz_menu
 from quiz_create import display_quizzes
 from quiz_create import get_quizzes
+from login import login
+from login import create_profile
+from login import delete_profile
 
 # Gives user the question and grades them
 def give_quiz(questions):
@@ -23,26 +26,61 @@ def give_quiz(questions):
 
 # Runs the main menu and enters the quiz
 def main():
+
+    is_admin = login()
+
     while True:
         print("-~-~-~-~- THE QUIZ GAME -~-~-~-~-")
 
-        print("1. Take Quiz \n2. Create Quiz \n3. Exit")
-        
-        selection = input("What would you like to do? (Enter Number) ")
+        if not is_admin:
+            print("1. Take Quiz \n2. Exit")
+            
+            selection = input("What would you like to do? (Enter Number) ")
 
-        if selection == "3":
-            print('Exiting')
-            break
-        elif selection == '2':
-            create_quiz_menu()
-        elif selection == '1':
+            if selection == "2":
+                print('Exiting')
+                break
+            elif selection == '1':
 
-            display_quizzes(False)
+                display_quizzes(False)
 
-            try:
-                questions = randomize_questions(get_questions(get_quizzes(False)[int(input('What quiz would you like to take? (Enter Number) ')) - 1]))
-                print(give_quiz(questions))
-            except:
-                print(f'{red}Invalid Input{reset}')
+                try:
+                    questions = randomize_questions(get_questions(get_quizzes(False)[int(input('What quiz would you like to take? (Enter Number) ')) - 1]))
+                    print(give_quiz(questions))
+                except:
+                    print(f'{red}Invalid Input{reset}')
+        else:
+            print("1. Take Quiz \n2. Create Quiz \n3. Create Profile \n4. Delete Profile \n5. Exit")
+            
+            selection = input("What would you like to do? (Enter Number) ")
+
+            # Exit
+            if selection == "5":
+                print('Exiting')
+                break
+            # Create Profile
+            elif selection == "3":
+                match input('1. User \n2. Admin \nWhat will the new profile be?'):
+                    case '1':
+                        create_profile(False)
+                    case '2':
+                        create_profile(True)
+            # Delete Profile
+            elif selection == '4':
+                delete_profile()
+            # Create Quiz
+            elif selection == '2':
+                create_quiz_menu()
+
+            # Take Quiz
+            elif selection == '1':
+
+                display_quizzes(False)
+
+                try:
+                    questions = randomize_questions(get_questions(get_quizzes(False)[int(input('What quiz would you like to take? (Enter Number) ')) - 1]))
+                    print(give_quiz(questions))
+                except:
+                    print(f'{red}Invalid Input{reset}')
 
 main()
