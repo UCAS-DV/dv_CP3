025d1fcab939ed7d123a7897cce8ba321f73e89d
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-08-26 11:48:37 -0600

    started user_login

--- projects/august/user_login/main.cpp
+++ projects/august/user_login/main.cpp
@@ -5,20 +5,13 @@ using namespace std;
 
 int main(){
 
-    string usernames[4] = {"ImagineExisting", "Darius", "Cinnamon", "Jerld"};
-    string status[4] = {"Admin", "Admin", "User", "User"};
+    string usernames[4] = {"Admin-ImagineExisting", "Admin-Darius", "User-Cinnamon", "User-Jerld"};
 
     string username = "";
+    int user_index = 0;
 
     cout << "Enter Username: ";
     cin >> username;
-
-    if (find(begin(usernames), end(usernames), username) != end(usernames)){
-        cout << "Real!";
-    } 
-    else {
-        cout << "Lets get you signed up " << username << "." << endl;
-    }
 
     return 0;
 }
