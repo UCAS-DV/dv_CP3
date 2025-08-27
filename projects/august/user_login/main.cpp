6f4bb3efefee500ebb474b8a534795e19d38a683
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-08-27 11:14:47 -0600

    added comments to user_login

--- projects/august/user_login/main.cpp
+++ projects/august/user_login/main.cpp
@@ -14,6 +14,7 @@ int main(){
     cout << "Enter Username: ";
     cin >> username;
 
+    // Loops through admins to see whether the username is in admins array
     for (int i = 0; i < 2; i++){
         if (username == admins[i]){
             cout << "Hello Admin " << username;
@@ -21,6 +22,7 @@ int main(){
         }
     }
 
+    // Loops through users to see whether the username is in users array
     for (int i = 0; i < 3; i++){
         if (username == users[i]){
             cout << "Welcome back " << username;
@@ -28,6 +30,7 @@ int main(){
         }
     }
 
+    // IF username not in admins or users, print this:
     cout << "Let's get you signed up " << username;
     return 0;
 }
