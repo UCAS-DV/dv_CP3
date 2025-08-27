d4c2b8ce770c293eb0837e08eb0009029156cace
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-08-27 11:48:45 -0600

    asf

---
+++ notes/august/arrays/arrays.cpp
@@ -0,0 +1,14 @@
+// DV Array Notes
+
+#include <iostream>
+using namespace std;
+
+string fam[5] = {"Darius", "Jaelen", "Liam", "Marisa", "Lahie"};
+
+int main(){
+    for (int i = 0; i < size(fam); i++){
+        cout << fam[i] << " Vaiaoga\n";
+    }
+
+    return 0;
+}
