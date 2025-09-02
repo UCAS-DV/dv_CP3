0cbf06e725129ab754c5fc9cda6884cc919deadb
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-09-02 11:50:41 -0600

    created way to check if player or cpu won

--- projects/august/tic_tac_toe/main.cpp
+++ projects/august/tic_tac_toe/main.cpp
@@ -13,12 +13,11 @@ char numbered_board[3][3] = {
     {'7','8','9'}
 };
 
-int turn = -1;
-int game_state = 0;
-
-bool take_space(int space, int player){
+bool take_space(int space, char player){
+    // IF space is taken, return false
     if (board[space / 3][space - ((space / 3) * 3)] != '-'){
         return false;
+    // ELSE, take space for player and return true
     } else{
         board[space / 3][space - ((space / 3) * 3)] = player;
         return true;
@@ -46,9 +45,38 @@ void print_board(bool numbered){
     }
 }
 
+int check_win(char player){
+    char completed_row[] = {player, player, player};
+
+    // CHECK each row. IF row equals completed row, return 1
+    for (int i = 0; i < 3; i++){
+        char board_row[] = {board[i][0], board[i][1], board[i][2]};
+
+        if (board_row == completed_row)
+            return 1;
+                    
+    }
+
+    for (int i = 0; i < 3; i++){
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] == player)
+            return 1;    
+        }
+
+    if (board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] == player)
+        return 1;
+    else if (board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] == player)
+        return 1;
+
+    return 0;
+}
+
+
+
 int main(){
 
     string blank_input;
+    int turn = -1;
+    int game_state = 0;
 
     cout << "-~-~-~-~-~-~-~-~Tic/Tac/Toe-~-~-~-~-~-~-~-~\n";
 
@@ -56,13 +84,57 @@ int main(){
     srand(seconds);
     int my_num = rand() % 11;
 
-    if (turn = -1){
-        cout << "If you ever need to be reminded of the space number. Type '-1' to get the numbered board\n";
-        print_board(true);
+    while (game_state == 0){
+        if (turn = -1){
+            cout << "If you ever need to be reminded of the space number. Type '-1' to get the numbered board\n";
+            print_board(true);
 
-        cout << "If you understand, enter anything. ";
-        cin >> blank_input;
+            cout << "If you understand, enter anything. ";
+            cin >> blank_input;
+
+            turn = 0;
+        }
+    
+        if (turn % 2 == 0){
+            int player_move;
+
+            cout << "What space would you like to take: ";
+            cin >> player_move;
+
+            if (player_move == -1){
+                print_board(true);
+                continue;
+            }
+
+            if (take_space(player_move, 'X') == false){
+                cout << "Oops! Seems like that space is taken! Please try again.";
+                continue;
+            } else{
+                game_state == check_win('X');
+                turn += 1;
+            }
+        } else {
+
+            int seconds = time(nullptr);
+            srand(seconds);
+            int rand_int = rand() % 9;
+
+            if (take_space(rand_int,'O') == false)
+                continue;
+            else {
+                game_state = check_win('O');
+
+                turn += 1;
+            }
+        }
+
+        print_board(false);
     }
+
+    if (game_state == 1)
+        cout << "You Won!";
+    else if (game_state == 2)
+        cout << "CPU Won!";
 
     return 0;
 }
