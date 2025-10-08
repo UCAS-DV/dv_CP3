fff42726af28d0d7e9e6115328e625be07a39f65
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-10-08 11:48:19 -0600

    literally 1984

--- projects/october/movie_library/main.cpp
+++ projects/october/movie_library/main.cpp
@@ -1,10 +1,86 @@
 #include <iostream>
+#include <limits>
+#include <fstream>
+#include <iomanip>
+#include <istream>
+#include <vector>
 
 using namespace std;
 
+struct Movie {
+    string name;
+    string director;
+    int year;
+    string genre1;
+    string genre2;
+    string rating;
+};
+
+enum Options {
+    View = 1,
+    Add,
+    Delete,
+    Search
+};
+
 int main(){
+    ifstream file;
+    file.open("movies.csv");
+    string line;
+    vector<Movie> movies;
 
+    if(file.is_open()){
+        
+        getline(file,line);
+
+        while (getline(file,line)){
+            istringstream iss(line);
+            string item;
+            Movie movie;
+
+            getline(iss, item, ',');
+            movie.name = item;
+            getline(iss, item, ',');
+            movie.director = item;
+            getline(iss, item, ',');
+            movie.year = stoi(item);
+            getline(iss, item, ',');
+            movie.genre1 = item;
+            getline(iss, item, '/');
+            movie.genre2 = item;
+            getline(iss, item, ',');
+            movie.rating = item;
+
+            movies.push_back(movie);
+        }
+    }
     
+    file.close();
 
+    bool open = true;
+
+    while (open){
+        int selected_option;
+
+        cout << "1. View All Movies\n2. Add Movie\n3. Delete Movie\n4. Search Movies\nEnter the number of the option to select: ";
+        cin >> selected_option;
+
+        switch (selected_option){
+            case Options::View:{
+                for (Movie movie : movies){
+                    cout << movie.name << endl;
+                }
+            }
+            case Options::Add:{
+
+            }
+            case Options::Delete:{
+
+            }
+            case Options::Search:{
+
+            }
+        }
+    }
     return 0;
 }
